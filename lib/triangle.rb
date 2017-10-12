442fc70a0d87d0c0b6488752de12b4f7133546ff
Dakota Lillie <dakota.lillie@icloud.com>
2017-10-12 18:33:05 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if triangleValid?
+      if self.a == self.b && self.a == self.c
+        #all sides equal
+        :equilateral
+      elsif self.a == self.b || self.a == self.c || self.b == self.c
+        #two sides equal
+        :isosceles
+      elsif self.a != self.b && self.a != self.c && self.b != self.c
+        #no sides equal
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  def triangleValid?
+    triangle_inequality = ((self.a + self.b) > self.c) && ((self.a + self.c) > self.b) && ((self.b + self.c) > self.a)
+    sides_exist = (self.a > 0) && (self.b > 0) && (self.c > 0)
+    triangle_inequality && sides_exist
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    "This is not a valid triangle"
+  end
 end
