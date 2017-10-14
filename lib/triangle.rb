5b1841dc59571e31149050779eea41b2c7f9447a
github username <github email address>
2017-10-14 12:37:05 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if valid
+      if self.side1 == self.side2 && self.side1 == self.side3
+         :equilateral
+      elsif self.side1 == self.side2 || self.side2 == self.side3 || self.side1 == self.side3
+         :isosceles
+      else
+         :scalene
+       end
+    else
+      raise TriangleError
+    end
+  end
+
+  def valid
+    inequality = (self.side1 + self.side2) > self.side3 && (self.side1 + self.side3) > side2 && (self.side2 + self.side3) > self.side1
+    isnotzero = self.side1 > 0 && self.side2 > 0 && self.side3 > 0
+    inequality && isnotzero
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "Triangle sides can not be zero and must follow triangle inequality!"
+  end
 end
